80dbccfaaab71ccadf5615bfb57364235d4c7d37
Pej's Houseboat <pejshouseboat@Pejis-MacBook-Air.local>
2022-11-05 17:14:49 +0000

    Initial commit, logo splash screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="App-logo">
+        CAREER<br />
+        WE<br />
+        GO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-sub-header">
+        THE FOOTBALL TRIVIA WEBSITE
+        </p>
       </header>
     </div>
   );
