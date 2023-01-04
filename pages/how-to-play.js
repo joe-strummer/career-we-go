10d72010cd5927b21349f748bf7ef8f0a04894e5
Peji <peji.faghihi+github@gmail.com>
2023-01-04 15:24:24 +0000

    new options

---
+++ pages/how-to-play.js
@@ -0,0 +1,16 @@
+export default function HowToPlay() {
+    return (
+        <div>
+            <h1>How To Play</h1>
+            <p>
+                Club by club a player's career stats will be revealed.
+            </p>
+            <p>
+                When you think you know, check by pressing Answer.
+            </p>
+            <p>
+                You're playing against the clock, so be sure to share with us how well you did afterwards!
+            </p>
+        </div>
+    );
+}
